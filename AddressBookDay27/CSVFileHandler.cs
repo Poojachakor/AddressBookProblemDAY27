5944d212b4f8b02d1f1a44b095431b9c72e273d3
Poojachakor <chakorpooja07@gmail.com>
2023-04-04 20:57:52 +0530

    Add: UC15 file

--- AddressBookDay27/CSVFileHandler.cs
+++ AddressBookDay27/CSVFileHandler.cs
@@ -9,42 +9,42 @@ using System.Threading.Tasks;
 
 namespace AddressBookDay27
 {
-    
-    
-        public class CSVFileHandler
+
+
+    public class CSVFileHandler
+    {
+        private string filePath = @"C:\Users\hp\source\repos\AddressBookDay27\AddressBookDay27\Utility\AddressBookCsvFile.csv";
+        public void WriteToFile(Dictionary<string, AddressBookManagement> addressBookDictionary)
         {
-            private string filePath = @"C:\Users\hp\source\repos\AddressBookDay27\AddressBookDay27\Utility\AddressBookCsvFile.csv";
-            public void WriteToFile(Dictionary<string, AddressBookManagement> addressBookDictionary)
+            using (StreamWriter writer = new StreamWriter(filePath))
             {
-                using (StreamWriter writer = new StreamWriter(filePath))
+                using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
                 {
-                    using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    foreach (AddressBookManagement obj in addressBookDictionary.Values)
                     {
-                        foreach (AddressBookManagement obj in addressBookDictionary.Values)
-                        {
-                            List<Contact> contactRecord = obj.addressBook.Values.ToList();
-                            csv.WriteRecords(contactRecord);
-                        }
-                        Console.WriteLine("\nSuccessfully added to CSV file.");
-                        csv.Dispose();
+                        List<Contact> contactRecord = obj.addressBook.Values.ToList();
+                        csv.WriteRecords(contactRecord);
                     }
+                    Console.WriteLine("\nSuccessfully added to CSV file.");
+                    csv.Dispose();
                 }
             }
-            public void ReadFromFile()
+        }
+        public void ReadFromFile()
+        {
+            using (StreamReader reader = new StreamReader(filePath))
             {
-                using (StreamReader reader = new StreamReader(filePath))
+                using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
                 {
-                    using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    Console.WriteLine("Below are Contents of CSV File");
+                    List<Contact> contactRecord = csv.GetRecords<Contact>().ToList();
+                    foreach (Contact contact in contactRecord)
                     {
-                        Console.WriteLine("Below are Contents of CSV File");
-                        List<Contact> contactRecord = csv.GetRecords<Contact>().ToList();
-                        foreach (Contact contact in contactRecord)
-                        {
-                            Console.WriteLine(contact.ToString());
-                        }
+                        Console.WriteLine(contact.ToString());
                     }
                 }
             }
         }
-    
+    }
+
 }
---
+++ AddressBookDay27/JsonOperation.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay27
+{
+    class JsonOperation
+    {
+        string filePath = @"C:\Users\hp\source\repos\AddressBookDay27\AddressBookDay27\Utility\AddressBookRecord.json";
+        public void WriteToFile(Dictionary<string, AddressBookManagement> addressBookDictionary)
+        {
+            string json = "";
+            foreach (AddressBookManagement obj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in obj.addressBook.Values)
+                {
+                    json = JsonConvert.SerializeObject(contact);
+                    File.WriteAllText(filePath, json);
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to JSON file.");
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine("Below are Contents of JSON File");
+            var json = File.ReadAllText(filePath);
+            Contact contact = JsonConvert.DeserializeObject<Contact>(json);
+            Console.WriteLine(contact.ToString());
+        }
+    }
+}
+
