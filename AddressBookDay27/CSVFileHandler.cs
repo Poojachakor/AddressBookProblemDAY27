7ddf40589264a169c43941e4c68be1f54bd29caa
Poojachakor <chakorpooja07@gmail.com>
2023-04-04 20:38:57 +0530

    Add: UC14 file

---
+++ AddressBookDay27/CSVFileHandler.cs
@@ -0,0 +1,50 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay27
+{
+    
+    
+        public class CSVFileHandler
+        {
+            private string filePath = @"C:\Users\hp\source\repos\AddressBookDay27\AddressBookDay27\Utility\AddressBookCsvFile.csv";
+            public void WriteToFile(Dictionary<string, AddressBookManagement> addressBookDictionary)
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    using (CsvWriter csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        foreach (AddressBookManagement obj in addressBookDictionary.Values)
+                        {
+                            List<Contact> contactRecord = obj.addressBook.Values.ToList();
+                            csv.WriteRecords(contactRecord);
+                        }
+                        Console.WriteLine("\nSuccessfully added to CSV file.");
+                        csv.Dispose();
+                    }
+                }
+            }
+            public void ReadFromFile()
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        Console.WriteLine("Below are Contents of CSV File");
+                        List<Contact> contactRecord = csv.GetRecords<Contact>().ToList();
+                        foreach (Contact contact in contactRecord)
+                        {
+                            Console.WriteLine(contact.ToString());
+                        }
+                    }
+                }
+            }
+        }
+    
+}
--- AddressBookDay27/FileIOOperations.cs
+++ AddressBookDay27/FileIOOperations.cs
@@ -6,29 +6,29 @@ using System.Threading.Tasks;
 
 namespace AddressBookDay27
 {
-   
-        public class FileIOOperations
+
+    public class FileIOOperations
+    {
+        private string filePath = @"C:\Users\hp\source\repos\AddressBookDay27\AddressBookDay27\Utility\AddressRecordBook.txt";
+        public void WriteToFile(Dictionary<string, AddressBookManagement> addressBookDictionary)
         {
-            private string filePath = @"C:\Users\hp\source\repos\AddressBookDay27\AddressBookDay27\Utility\AddressRecordBook.txt";
-            public void WriteToFile(Dictionary<string, AddressBookManagement> addressBookDictionary)
+            using StreamWriter writer = new StreamWriter(filePath, true);
+            foreach (AddressBookManagement addressBookobj in addressBookDictionary.Values)
             {
-                using StreamWriter writer = new StreamWriter(filePath, true);
-                foreach (AddressBookManagement addressBookobj in addressBookDictionary.Values)
+                foreach (Contact contact in addressBookobj.addressBook.Values)
                 {
-                    foreach (Contact contact in addressBookobj.addressBook.Values)
-                    {
-                        writer.WriteLine(contact.ToString());
-                    }
+                    writer.WriteLine(contact.ToString());
                 }
-                Console.WriteLine("\nSuccessfully added to Text file.");
-                writer.Close();
             }
-            public void ReadFromFile()
-            {
-                Console.WriteLine("Below are Contents of Text File");
-                string lines = File.ReadAllText(filePath);
-                Console.WriteLine(lines);
-            }
+            Console.WriteLine("\nSuccessfully added to Text file.");
+            writer.Close();
         }
-    
+        public void ReadFromFile()
+        {
+            Console.WriteLine("Below are Contents of Text File");
+            string lines = File.ReadAllText(filePath);
+            Console.WriteLine(lines);
+        }
+    }
+
 }
